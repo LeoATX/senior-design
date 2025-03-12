a497c96167925b5a167cb6551b6608c46da7f53e
Leo <Leo_Chen_zy@hotmail.com>
2025-03-12 10:55:32 -0700

    Deleted ROS Scripts and added cuda setup scripts

--- setup/install_python3_11.sh
+++ setup/install_python3_11.sh
@@ -7,7 +7,8 @@ wget https://www.python.org/ftp/python/3.11.11/Python-3.11.11.tgz
 tar xzf Python-3.11.11.tgz
 cd Python-3.11.11
 
-sudo ./configure --prefix=/opt/python/ --enable-optimizations --with-lto --with-computed-gotos --with-system-ffi --with-openssl=/usr/lib/ssl
+sudo ./configure --prefix=/opt/python/ --enable-optimizations --with-lto --with-computed-gotos --with-system-ffi --with-openssl=/usr/bin/
+# tried --with-openssl= /usr/lib/, /usr/include/
 sudo make -j "$(grep -c ^processor /proc/cpuinfo)"
 sudo make altinstall
 sudo rm /tmp/Python-3.11.11.tgz
